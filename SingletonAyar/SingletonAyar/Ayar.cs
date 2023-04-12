51bbb815f52e6c7f9e063a8a098619ef089adedc
Mehmet Kizilboga <mehmet.kizilboga@icloud.com>
2023-04-12 15:31:39 +0300

    Proje Sisteme Yüklendi.

---
+++ SingletonAyar/SingletonAyar/Ayar.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signleton
+{
+    public class Ayar
+    {
+        private static Ayar ornek;
+
+        private int ses = 0;
+
+        private int parlaklik = 0;
+
+        public static Ayar SesAyari
+        {
+            get
+            {
+                if (ornek == null)
+                {
+                    ornek = new Ayar();
+                }
+                return ornek;
+            }
+        }
+
+        private Ayar()
+        {
+            Console.WriteLine("Ayar adında nesne örneği oluştu.");
+        }
+
+        public void SesArttir(int miktar)
+        {
+            ses += miktar;
+        }
+
+        public void SesAzalt(int miktar)
+        {
+            ses -= miktar;
+        }
+
+        public void ParlaklikArttir(int miktar)
+        {
+            parlaklik += miktar;
+        }
+
+        public void ParlaklikAzalt(int miktar)
+        {
+            parlaklik -= miktar;
+        }
+
+        public void SesDegerGoster()
+        {
+            Console.WriteLine($"Şuanda ses miktarı : {ses}");
+        }
+
+        public void ParlaklikDegerGoster()
+        {
+            Console.WriteLine($"Şuanda parlaklık miktarı : {parlaklik}");
+        }
+    }
+}
---
+++ SingletonAyar/SingletonAyar/Program.cs
@@ -0,0 +1,58 @@
+﻿using Signleton;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonAyar
+{
+
+    /*
+     Tekillik dokusu (Singleton pattern): Bir sınıftan program çalışırken sadece bir örnek oluşturulması isteniyorsa bu doku kullanılabilir. 
+    Örnek : Bir uygulamanın tekrar tekrar açılmasını engellemek için.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ayar ayarSiniffOrnegi1 = Ayar.SesAyari;
+            Ayar ayarSiniffOrnegi2 = Ayar.SesAyari;
+
+            Console.WriteLine("**************************************************");
+
+            ayarSiniffOrnegi1.SesArttir(5);
+            ayarSiniffOrnegi1.SesDegerGoster();
+
+            ayarSiniffOrnegi1.SesArttir(2);
+            ayarSiniffOrnegi1.SesDegerGoster();
+
+            ayarSiniffOrnegi1.SesAzalt(1);
+            ayarSiniffOrnegi1.SesDegerGoster();
+
+            Console.WriteLine("**************************************************");
+
+            ayarSiniffOrnegi1.ParlaklikArttir(5);
+            ayarSiniffOrnegi1.ParlaklikDegerGoster();
+
+            ayarSiniffOrnegi1.ParlaklikArttir(2);
+            ayarSiniffOrnegi1.ParlaklikDegerGoster();
+
+            ayarSiniffOrnegi1.ParlaklikAzalt(1);
+            ayarSiniffOrnegi1.SesDegerGoster();
+
+            Console.WriteLine("**************************************************");
+
+            ayarSiniffOrnegi2.SesArttir(5);
+            ayarSiniffOrnegi1.SesDegerGoster();
+
+            ayarSiniffOrnegi1.ParlaklikArttir(5);
+            ayarSiniffOrnegi1.ParlaklikDegerGoster();
+
+            Console.WriteLine("**************************************************");
+
+            Console.Read();
+
+        }
+    }
+}
